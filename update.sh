79a413acd06d2386345c76d5499019ff57f7d647
WebDate8 <121506083+WebDate8@users.noreply.github.com>
2024-01-18 20:22:17 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,6 +7,5 @@ git clone https://github.com/webdate8/zshrc
 rm .zshrc
 mv ~/zshrc/.zshrc ~/.zshrc
 rm -rf zshrc
-rm -rf a.sh
 
 echo "Pansiere - Configurao concluida. Reinicie o terminal para aplicar."
