67c892630dbaafd24bf0a27ea38e2619f405d5b1
WebDate8 <121506083+WebDate8@users.noreply.github.com>
2024-03-18 23:52:04 -0300

    Atualizar o tmux-conf.sh

--- tmux-conf.sh
+++ tmux-conf.sh
@@ -1,1 +1,11 @@
-A
+#!/bin/bash
+
+cd ~/
+
+git clone https://github.com/webdate8/zshrc
+
+rm .zshrc
+mv ~/zshrc/.tmux.conf ~/.tmux.conf
+rm -rf zshrc
+
+echo "Pansiere - Configurao concluida. Reinicie o terminal para aplicar."
